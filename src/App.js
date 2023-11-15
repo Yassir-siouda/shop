11d3ec2d68af2344ac182c3f8d8f39fef087ade6
Yassir-siouda <72074295+Yassir-siouda@users.noreply.github.com>
2023-11-15 23:29:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const LoginForm = ({ onLogin, onLogout, isLoggedIn }) => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Ajoutez votre logique d'authentification ici en utilisant formData
+    onLogin(formData.username); // Appel de la fonction de connexion avec le nom d'utilisateur
+  };
+
+  const handleLogout = () => {
+    // Ajoutez votre logique de déconnexion ici
+    onLogout();
+  };
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <div>
+          <p>Bienvenue, {formData.username}!</p>
+          <button onClick={handleLogout}>Déconnexion</button>
+        </div>
+      ) : (
+        <div>
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Username:
+              <input
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={handleInputChange}
+              />
+            </label>
+            <br />
+            <label>
+              Password:
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+            </label>
+            <br />
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+};
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      message: 'Hello, World!'
+      isLoggedIn: false,
+      username: '',
     };
   }
+
+  handleLogin = (user) => {
+    // Ajoutez votre logique d'authentification ici
+    this.setState({ username: user, isLoggedIn: true });
+  };
+
+  handleLogout = () => {
+    // Ajoutez votre logique de déconnexion ici
+    this.setState({ username: '', isLoggedIn: false });
+  };
 
   render() {
     return (
       <div>
-        <h1>Hello word !!!!!</h1>
+        <h1>Votre application</h1>
+        <LoginForm
+          onLogin={this.handleLogin}
+          onLogout={this.handleLogout}
+          isLoggedIn={this.state.isLoggedIn}
+        />
       </div>
     );
   }
